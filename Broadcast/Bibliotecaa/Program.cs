2ff8c5ca240cdea59b6488f530039fa4a04dcbb7
xAKLx <xaklx@hotmail.com>
2016-04-11 02:47:08 -0400

    Desesperacion

---
+++ Broadcast/Bibliotecaa/Program.cs
@@ -0,0 +1,27 @@
+﻿using SocketClientExample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsynchronousClient client = new AsynchronousClient(Dns.GetHostName(), 11000);
+            new Thread(new ThreadStart(AsynchronousClient.StartClient)).Start();
+
+            string msg;
+            while (true)
+            {
+                msg = Console.ReadLine();
+                AsynchronousClient.SendData(msg);
+            }
+        }
+    }
+}
---
+++ Broadcast/Bibliotecaa/StateObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+// State object for receiving data from remote device.
+public class StateObject
+{
+    // Client socket.
+    public Socket workSocket = null;
+    // Size of receive buffer.
+    public const int BufferSize = 256;
+    // Receive buffer.
+    public byte[] buffer = new byte[BufferSize];
+    // Received data string.
+    public StringBuilder sb = new StringBuilder();
+}
